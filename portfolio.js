a9f5268097e9e4ef1fb62a666199df33e6e0f90c
AishatulHani12 <d20221101861@m365.upsi.edu.my>
2024-11-23 16:26:07 +0800

    FirstCommit

---
+++ portfolio.js
@@ -0,0 +1,45 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Handle scroll and active link
+window.addEventListener('scroll', () => {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100; // Adjust this value if needed
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // Active section for animation on scroll
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    // Sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+});
+
+// Remove toggle icon and navbar when a link is clicked
+let navLinks = document.querySelectorAll('header nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
+});
